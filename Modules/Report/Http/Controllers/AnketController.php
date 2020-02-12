8500586cd61c053cc8aed3694ec19414d9ee04c7
rai68 <rai68@yandex.ru>
2020-02-12 15:39:10 +0300

    блок анкет почти закончен

--- Modules/Report/Http/Controllers/AnketController.php
+++ Modules/Report/Http/Controllers/AnketController.php
@@ -5,9 +5,12 @@ namespace Modules\Report\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 use Modules\Admin\Entities\Role;
 use Modules\Marketing\Entities\Form;
 use Modules\Marketing\Entities\FormQty;
+use Modules\Marketing\Entities\Logform;
+use Modules\Marketing\Entities\Question;
 
 class AnketController extends Controller
 {
@@ -25,8 +28,35 @@ class AnketController extends Controller
             $start = $input['start'];
             $finish = $input['finish'];
             $form_id = $input['form_id'];
+            $data = array();
+            $pie = array();
             $qty = FormQty::where(['form_id'=>$form_id])->whereBetween('date', [$start, $finish])->sum('qty');
-            return $qty;
+            array_push($data,["qty"=>"$qty"]);
+            $content = '';
+            //определяем вопросы анкеты из логов
+            $rows = DB::select("SELECT DISTINCT `name` FROM (`questions` JOIN `logforms`  ON((`questions`.`id` = `logforms`.`question_id`))) WHERE `questions`.`form_id`=$form_id AND `data` BETWEEN '$start' AND '$finish' ");
+            $i=1;
+            foreach ($rows as $row) {
+                $question = Question::where(['form_id'=>$form_id,'name'=>$row->name])->first();
+                $sum = Logform::select('answer')->where(['question_id' => $question->id])->whereBetween('data',[$start,$finish])->count('answer');
+                $values = DB::select("select answer,count(answer) as kol,count(answer)/$sum as percent from logforms where question_id=$question->id 
+                                     and (`data` between '$start' and '$finish') group by answer order by kol DESC");
+                $content.= '<div class="col-md-offset-2 col-md-8"><div class="pie" id="pie-'.$i.'">График</div>';
+                $content.= '<button type="button" class="btn btn-default btn-sm"><i class="fa fa-expand fa-lg show" aria-hidden="true"></i></button><div class="other">';
+                $content.='<table class="table table-hover table-bordered"><tr><th>Ответ</th><th>Кол-во ответов</th><th>% ответов</th></tr>';
+                foreach ($values as $val){
+                    $tmp = array();
+                    $content.= '<tr><td>'.$val->answer.'</td><td>'.$val->kol.'</td><td>'.round($val->percent*100,2).'</td></tr>';
+                    $tmp['answer'] = $val->answer;
+                    $tmp['kol'] = $val->kol;
+                    array_push($pie,$tmp);
+                }
+                $content .= '</table></div></div>';
+                $i++;
+            }
+            array_push($data,["content"=>$content]);
+            array_push($data,["pie"=>$pie]);
+            return json_encode($data);
         }
         if(view()->exists('report::anket')){
             $title='Анкетирование';
--- Modules/Report/Resources/views/anket.blade.php
+++ Modules/Report/Resources/views/anket.blade.php
@@ -12,6 +12,7 @@
     </ul>
     <!-- END BREADCRUMB -->
     <!-- page content -->
+    <div id="loader"></div> <!--  идентификатор загрузки (анимация) - ожидания выполнения-->
     <div class="row">
         <div class="col-md-12">
             <h2 class="text-header text-center">{{ $head }}</h2>
@@ -57,11 +58,10 @@
                 {!! Form::close() !!}
             </div>
             <a href="#"
-               onclick="$('.x_content').show(); $('#result').hide(); $('.fa-plus-square-o').hide(); return false;"><i
+               onclick="$('.x_content').show(); $('#result').hide(); $('.fa-plus-square-o').hide(); $('p.text-header').text(''); $('h2.text-header').text('Задайте условия отбора'); return false;"><i
                     class="fa fa-plus-square-o fa-lg" aria-hidden="true"></i></a>
             <div class="x_panel" id="result">
-                <div class="pull-center" style="width: 50%; height: 400px;" id="graph_pie"></div>
-                <div id="table-data"></div>
+
             </div>
 
         </div>
@@ -72,66 +72,82 @@
 
 @section('user_script')
     <script src="/js/select2.min.js"></script>
+    <script src="/js/raphael.min.js"></script>
+    <script src="/js/morris.min.js"></script>
     <script src="/js/gstatic_charts_loader.js"></script>
     <script>
         $('.select2').css('width', '100%').select2({allowClear: false})
         $('#result').hide();
         $('.fa-plus-square-o').hide();
+        let c = 0;
+        $('.other').hide(); //скрыли значения в таблице вывода анкеты
+
+        $(document).on({
+            click: function () {
+                $(this).removeClass('fa-expand');
+                $(this).addClass('fa-compress');
+                $(this).parent().next().show();
+
+            }
+        }, ".fa-expand");
+
+        $(document).on({
+            click: function () {
+                $(this).removeClass('fa-compress');
+                $(this).addClass('fa-expand');
+                $(this).parent().next().hide();
+            }
+        }, ".fa-compress");
+
         $('#report').click(function (e) {
             e.preventDefault();
+            $("#loader").show();
             $.ajax({
                 url: '{{ route('anket-report') }}',
                 type: 'POST',
                 data: $('#new_val').serialize(),
                 success: function (res) {
-                    $("p.text-header").text('за период с '+$('#start').val()+' по '+$('#finish').val()+' опрошено '+res+' человек');
+                    let obj = jQuery.parseJSON(res);
+                    $("p.text-header").text('за период с ' + $('#start').val() + ' по ' + $('#finish').val() + ' опрошено ' + obj[0].qty + ' человек');
                     $('#result').show();
                     //alert("Сервер вернул вот что: " + res);
-                    $("#graph_pie").empty();
-                    //alert("Сервер вернул вот что: " + res);
-                    var obj = jQuery.parseJSON(res);
-                    google.charts.load('current', {'packages': ['corechart']});
-                    google.charts.setOnLoadCallback(drawChart);
-
-                    function drawChart() {
-                        var data = new google.visualization.DataTable();
-                        data.addColumn('string', 'counter');
-                        data.addColumn('number', 'cost');
-                        $.each(obj, function (key, value) {
-                            data.addRow([
-                                value.name,
-                                parseFloat(value.delta),
-                            ]);
-                        });
-                        var options = {
-                            is3D: true,
-                        };
-                        var chart = new google.visualization.PieChart(document.getElementById('graph_pie'));
-                        chart.draw(data, options);
-                    }
-                    $.ajax({
-                        url: '{{ route('rent-table') }}',
-                        type: 'POST',
-                        data: {'year': year},
-                        headers: {
-                            'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-                        },
-                        success: function (res) {
-                            //alert("Сервер вернул вот что: " + res);
-                            $("#table-data").html(res);
-                        },
-                        error: function (xhr, response) {
-                            alert('Error! ' + xhr.responseText);
+                    //заполняем графики
+                    let objpie = obj[2].pie;
+                    $.each(objpie,function(index,val) {
+                        google.charts.load('current', {'packages': ['corechart']});
+                        google.charts.setOnLoadCallback(drawChart);
+                        function drawChart() {
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', 'name');
+                            data.addColumn('number', 'kol');
+                            $.each(val, function(key,value) {
+                                alert(value.answer+' | '+value.kol);
+                                data.addRow([
+                                    value.answer,
+                                    parseInt(value.kol),
+                                ]);
+                            });
+                            var options = {
+                                is3D: true,
+                            };
+                            let idx = index;
+                            idx++;
+                            var chart = new google.visualization.PieChart(document.getElementById('pie-1'));
+                            chart.draw(data, options);
                         }
+                        //console.log('Индекс: ' + index + '; Значение: ' + val);
                     });
+                    $('#result').html(obj[1].content);
                     $("h2.text-header").text($('#form_id').text());
                     $('.x_content').hide();
+                    $('.other').hide(); //скрыли значения в таблице вывода анкеты
                     $('.fa-plus-square-o').show();
                 },
                 error: function (xhr, response) {
                     alert('Error! ' + xhr.responseText);
                 }
             });
+            $("#loader").hide();
         });
 
     </script>
