952041c42a320e8c4624ee78155079d9a60b7d64
dervish <rai68@yandex.ru>
2020-02-25 21:21:25 +0300

    осталось записать анкеты в базу

--- Modules/Marketing/Resources/views/form_view.blade.php
+++ Modules/Marketing/Resources/views/form_view.blade.php
@@ -16,8 +16,10 @@
     <div id="loader"></div> <!--  идентификатор загрузки (анимация) - ожидания выполнения-->
     <div class="row">
         <div class="col-md-offset-2 col-md-8">
+            {!! Form::open(['url' => '#','class'=>'form-horizontal','method'=>'POST']) !!}
             <h2 class="text-center">{{ $title }}</h2>
             {!! $content !!}
+            {!! Form::close() !!}
         </div>
     </div>
     </div>
@@ -25,72 +27,127 @@
 @endsection
 
 @section('user_script')
-<script>
-    $('.panel-info').hide();
-    $('select option:selected').each(function(){
-        this.selected=false;
-    });
-    $("select :first").attr("disabled", "disabled");
-    $('#qpanel1').show();
-    $('#save_btn').hide();
-    $('#prev_btn').hide();
+    <script>
+        $('.panel-info').hide();
+        $('select option:selected').each(function () {
+            this.selected = false;
+        });
+        $("select :first").attr("disabled", "disabled");
+        $('#qpanel1').show();
+        $('#save_btn').hide();
+        $('#prev_btn').hide();
 
-    let step = 1;
-    let prev_step = 0;
-    let next_step = 0;
-    let qcnt = $('#qst_count').val();
+        let step = 1;
+        let prev_step = 0;
+        let next_step = 0;
+        let qcnt = $('#qst_count').val();
 
-    $('#next_btn').click(function(e){
-        e.preventDefault();
-        prev_step = step;
-        if(step<qcnt){
-            $('#qpanel'+prev_step).hide();
-            step++;
-            $('#qpanel'+step).show();
-            let per = set_percent(step,qcnt);
-            $('.progress-bar').css('width',per+'%');
-            $('.progress-bar').text(per+'%');
+        $('#qpanel1').find(':radio').first().prop('checked', true);
+
+        $('input[type="radio"]').on('change', function (e) {
+            let stp = $(this).data('step');
+            let finish = $(this).data('finish');
+            if (typeof (stp) !== "undefined") {
+                step = stp - 1;
+            }
+            if (typeof (finish) !== "undefined") {
+                $('.progress-bar').css('width', '100%');
+                $('.progress-bar').text('100%');
+                $('#save_btn').show();
+                $('#next_btn').hide();
+            }
+        });
+
+        $('input[type="checkbox"]').on('change', function (e) {
+            let stp = $(this).data('step');
+            let finish = $(this).data('finish');
+            if (typeof (stp) !== "undefined") {
+                step = stp - 1;
+            }
+            if (typeof (finish) !== "undefined") {
+                $('.progress-bar').css('width', '100%');
+                $('.progress-bar').text('100%');
+                $('#save_btn').show();
+                $('#next_btn').hide();
+            }
+        });
+
+        $('#next_btn').click(function (e) {
+            e.preventDefault();
+            let err = 0;
+            $(".table").find(":input[name*='other']").each(function () {// проверяем каждое поле ввода в форме
+                if ($(this).prev().is(':checked')) { //если выбран чекбокс
+                    if (!$(this).val()) {// если поле пустое
+                        alert('Необходимо заполнить поле или выбрать значение из списка!');
+                        $(this).css('border', '1px solid red');// устанавливаем рамку красного цвета
+                        $(this).focus(); //установка фокуса на поле с ошибкой
+                        err = 1;
+                    } else {
+                        $(this).css('border', '');// устанавливаем рамку зеленого цвета
+                    }
+                }
+            })
+
+            if (!err) {
+                prev_step = step;
+                if (step < qcnt) {
+                    for (i = 1; i <= step; i++) {
+                        $('#qpanel' + i).hide();
+                    }
+                    step++;
+                    let obj = $('#qpanel' + step);
+                    obj.find(':radio').first().prop('checked', true);
+                    obj.find(':checkbox').first().prop('checked', true);
+                    $('#qpanel' + step).show();
+                    let per = set_percent(step, qcnt);
+                    $('.progress-bar').css('width', per + '%');
+                    $('.progress-bar').text(per + '%');
+                }
+                if (step == qcnt) {
+                    $('#save_btn').show();
+                    $('#next_btn').hide();
+                    $('.progress-bar').css('width', '100%');
+                    $('.progress-bar').text('100%');
+                }
+                if (step == 2)
+                    $('#prev_btn').show();
+            }
+        });
+
+        $('#prev_btn').click(function (e) {
+            e.preventDefault();
+            next_step = step;
+            let obj = $('#qpanel' + step);
+            // Снять все
+            obj.find(':radio').prop('checked', false);
+            obj.find(':checkbox').prop('checked', false);
+            if (next_step > 1) {
+                $('#qpanel' + next_step).hide();
+                step--;
+                $('#qpanel' + step).show();
+                let per = set_percent(step, qcnt);
+                $('.progress-bar').css('width', per + '%');
+                $('.progress-bar').text(per + '%');
+            }
+            if (step < qcnt) {
+                $('#save_btn').hide();
+                $('#next_btn').show();
+            }
+            if (step == 1) {
+                $('#prev_btn').hide();
+                $('.progress-bar').css('width', '2%');
+                $('.progress-bar').text('0%');
+            }
+        });
+
+        $('#save_btn').click(function (e) {
+            e.preventDefault();
+            alert('Сохраняем анкету');
+        });
+
+        function set_percent(a, b) {
+            return Math.round((a / b) * 100);
         }
-        if(step==qcnt){
-            $('#save_btn').show();
-            $('#next_btn').hide();
-            $('.progress-bar').css('width','100%');
-            $('.progress-bar').text('100%');
-        }
-        if(step == 2)
-            $('#prev_btn').show();
-    });
 
-    $('#prev_btn').click(function(e){
-        e.preventDefault();
-        next_step = step;
-        if(next_step > 1){
-            $('#qpanel'+next_step).hide();
-            step--;
-            $('#qpanel'+step).show();
-            let per = set_percent(step,qcnt);
-            $('.progress-bar').css('width',per+'%');
-            $('.progress-bar').text(per+'%');
-        }
-        if(step<qcnt){
-            $('#save_btn').hide();
-            $('#next_btn').show();
-        }
-        if(step == 1){
-            $('#prev_btn').hide();
-            $('.progress-bar').css('width','2%');
-            $('.progress-bar').text('0%');
-        }
-    });
-
-    $('#save_btn').click(function(e){
-        e.preventDefault();
-        alert('Сохраняем анкету');
-    });
-
-    function set_percent(a,b){
-        return Math.round((a/b)*100);
-    }
-
-</script>
+    </script>
 @endsection
